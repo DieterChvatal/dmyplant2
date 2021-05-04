99dab647b5ae4d961cf54d506361ba0efc270e95
DieterChvatal <dieter.chvatal@innio.com>
2021-05-04 17:41:25 +0200

    Test Commit

--- dmyplant2/dEngine.py
+++ dmyplant2/dEngine.py
@@ -21,7 +21,7 @@ class MyPlantException(Exception):
 class Engine:
     """
     Class to encapsulate Engine properties & methods
-    for MyPlant Access
+    for easy MyPlant Access
     """
     _sn = 0
     _picklefile = ''
