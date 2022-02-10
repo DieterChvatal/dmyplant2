2b8c2c682f322f1860cd672bf2bdc1dadff63b4b
DieterChvatal <dieter.chvatal@innio.com>
2022-02-10 17:34:26 +0100

    dengine2 cfile removed from __init__

--- dmyplant2/__init__.py
+++ dmyplant2/__init__.py
@@ -4,6 +4,5 @@ from dmyplant2.support import cred
 from dmyplant2.dMyplant import MyPlant, save_json, load_json
 from dmyplant2.dValidation import Validation, HandleID
 from dmyplant2.dEngine import Engine, Engine_SN
-from dmyplant2.dEngine2 import Engine2, Engine_SN2
 import dmyplant2.dReliability
 from dmyplant2.dPlot import demonstrated_Reliabillity_Plot, chart, scatter_chart, bokeh_chart, dbokeh_chart
